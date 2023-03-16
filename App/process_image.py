2850f98259a7f516b0aec2a052036c3e787ccb6c
gfumagali <guilhermefumarques@gmail.com>
2023-03-16 13:00:10 -0300

    faces detector

--- App/process_image.py
+++ App/process_image.py
@@ -16,13 +16,11 @@ classes = {
 
 def pre_process_image(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    """ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
-    print(len(faces))
-    if len(faces) == 0:
-        return None
-    x, y, w, h = faces[0]
-    img = img[y:y+h, x:x+w] """
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(48, 48))
+    if len(faces) > 0:
+        x, y, w, h = faces[0]
+        img = img[y:y + h, x:x + w]
     img = cv2.GaussianBlur(img, (5, 5), 0)
     img = cv2.resize(img, (48, 48), interpolation=cv2.INTER_CUBIC)
     img = cv2.equalizeHist(img)
