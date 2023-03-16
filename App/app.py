29ace9b250b9f3b347cf19ace62707ea23acb26b
gfumagali <guilhermefumarques@gmail.com>
2023-03-16 11:41:08 -0300

    API

--- App/app.py
+++ App/app.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify, request
 from process_image import get_emotion
 
+classes = {
+    'neutral': 0,
+    'happy': 1,
+    'sad': 2,
+    'surprised': 3,
+    'fearful': 4,
+    'disgusted': 5,
+    'angry': 6,
+    'contempt': 7
+}
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return jsonify({'message': 'Backend is working!'})
-
 
 @app.route('/image', methods=['POST'])
 def image():
